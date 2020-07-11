c42acd0e33cf062d151e7fe21f6e6c31a89ec8e8
Juan Bonforti <juan.bonforti@gmail.com>
2020-07-11 11:55:46 -0300

    Initial Commit

---
+++ FabricaAutomoviles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FabricaAutomoviles
+//
+//  Created by Juan Bonforti on 11/07/2020.
+//  Copyright © 2020 Juan Bonforti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
